287ba098fbc4fa5c101bafa7badba9433369e9d1
Lakshya Nigam <lakshyanigam20@gmail.com>
2026-05-05 00:57:47 +0530

    Add Tailwind practice

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-blue-500 text-white p-4 rounded'>Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
